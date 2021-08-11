cea75ba5c93df81cefc89562bdf224cb6319b78d
Afnanafsal <87473921+Afnanafsal@users.noreply.github.com>
2021-08-11 10:48:56 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN GROUP', url="https://t.me/allmoviesreqgroup"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
-    ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('⚡️CHANNEL⚡️', url='https://t.me/malik_chathurmugam'),
+]
+,[
+        InlineKeyboardButton('GROUP💥', url='https://t.me/allmoviesreqgroup')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
